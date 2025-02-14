bd05879782a5306c77730e54906669ea6f20168e
Hseyaj <codex.hseyaj26@gmail.com>
2025-02-14 18:26:20 +0530

    utils (Error API, Response API, Async Handler), done app.js configuration

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+})); // Cross-Origin Resource Sharing   
+
+app.use(express.json({limit: "16kb"})); // Parse JSON bodies
+app.use(express.urlencoded({extended: true, limit: "16kb"})); // Parse URL-encoded bodies
+app.use(express.static("public")); // Serve static files
+app.use(cookieParser()); // Parse cookie headers
+
+
+
+
+
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,33 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path : './env'
 });
-connectDB();
+connectDB().then(()=>{
+    app.on("error", (error) => {
+        console.log(`Error: ${error}`);
+    });
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+}).catch((error) => {
+    console.log(`Error: ${error}`);
+    process.exit(1);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 ;(async ()=>{

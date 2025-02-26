db4b780c1b578eeca04ac6516f7f190aa76a739a
Hseyaj <codex.hseyaj26@gmail.com>
2025-02-26 18:33:20 +0530

    add models for comments, playlists, tweets and likes with schema definitions

---
+++ src/models/tweet.model.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from "mongoose";
+
+const tweetSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    content: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+
+export const Tweet = new mongoose.model("Tweet", tweetSchema)
